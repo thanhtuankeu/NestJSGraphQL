ba30650820363165fa60dd61884faeb93e622cc6
zoro <zoro.dev@vmo.com>
2021-08-31 11:05:54 +0700

    add post request

--- src/controller/app.controller.ts
+++ src/controller/app.controller.ts
@@ -1,12 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { AppService } from 'src/service/app.service';
 
-@Controller()
+@Controller('app')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post()
+  postHello() {
+    return {
+      status: 'ok',
+      data: {
+        name: 'tuan',
+        age: '19',
+      },
+    };
   }
 }
