c57ede84be60e009cff4f42b2080519824dca23e
zoro <zoro.dev@vmo.com>
2021-08-31 11:25:30 +0700

    config heroku

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-
-  await app.listen(3000);
+  app.enableCors();
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
