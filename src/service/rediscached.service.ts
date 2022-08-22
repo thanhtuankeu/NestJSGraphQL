35b076c5dc97982018192faa02a6982eae2902f4
zorodev <zorodev@vmodev.com>
2022-08-22 13:47:01 +0700

    update minor code

--- src/service/rediscached.service.ts
+++ src/service/rediscached.service.ts
@@ -7,17 +7,24 @@ import { InjectRedis, Redis } from '@svtslv/nestjs-ioredis';
 
 @Injectable()
 export class RedisCachedService {
-  
   EXPIRE_TIME = 1000;
 
   constructor(@InjectRedis() private readonly redis: Redis) {}
 
   async get(key: string) {
-    const redisData = await this.redis.get(key);
-    return redisData;
+    try {
+      const redisData = await this.redis.get(key);
+      return redisData;
+    } catch (err) {
+      console.log(err);
+    }
   }
   async set(key: string, value: string): Promise<string> {
+    try {
+      return await this.redis.set(key, value, { ttl: this.EXPIRE_TIME });
+    } catch (err) {
+      console.log(err);
+    }
     console.log('set', key, value);
-    return this.redis.set(key, value, { ttl: this.EXPIRE_TIME });
   }
 }
